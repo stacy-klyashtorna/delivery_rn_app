53ea507c7ca86c9ed6b7e16a444212b85aab6585
Stacy Klyashtorna <anastasiya.klyashtorna@computools.com>
2021-06-09 16:52:16 +0300

    Added constants

---
+++ src/constants/colors.ts
@@ -0,0 +1,28 @@
+export const colors = {
+  headerTitle: '#000000',
+  border: '#CCCCCC',
+  greyBackground: '#f3f3f3',
+  warningBackground: '#EAEB5E',
+  warningText: '#666804',
+
+  black: '#000000',
+  off_black: '#00151E',
+  white: '#FFFFFF',
+
+  white_semi_transparent: 'rgba(255, 255, 255, 0.7)',
+
+  grey_100: '#BCBABA',
+  grey_200: '#E1E1E2',
+  grey_300: '#D0D0D2',
+  grey_400: '#BDBDBF',
+  grey_500: '#A1A1A6',
+  grey_700: '#7D7D82',
+  grey_900: '#283D46',
+
+  blue_500: '#0E73C0',
+  blue_800: '#00C8D7',
+
+  error_text: '#897306',
+
+  orange_400: '#FA4A0C',
+};
---
+++ src/constants/spacing.ts
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Ui Specification Variables
+ ******************************************************************************/
+const pixie = 8;
+const hobbit = 12;
+const dwarf = 16;
+const elf = 24;
+const horse = 32;
+const giant = 48;
+const dragon = 64;
+
+export const spacing = {
+  spacing8: pixie,
+  spacing12: hobbit,
+  spacing16: dwarf,
+  spacing24: elf,
+  spacing28: 28,
+  spacing32: horse,
+  spacing48: giant,
+  spacing64: dragon,
+
+  // Padding values for screens
+  screenPaddingHorizontal: elf,
+  screenPaddingVertical: elf,
+};
